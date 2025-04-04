9df4c78d2ec56928af198035284d3961262e8dbb
Han-Bin Song <thdgksq@naver.com>
2025-04-04 14:09:29 +0900

    auth.js 설정

---
+++ src/auth.ts
@@ -0,0 +1,31 @@
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+
+export const {
+  handlers,
+  signIn,
+  signOut,
+  auth,
+  unstable_update: update // Beta!
+} = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'Username' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize({ credentials, req }) {
+        const user = { id: '1', name: 'test', email: 'test@test.com' }
+
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  }
+})
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession()
+  console.log({ session }, status)
+
   return (
     <ul
       className={`text-md flex w-full items-center justify-center gap-4 ${mobile && 'h-full flex-col'}`}>
@@ -23,15 +27,25 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
         </Link>
       </li>
 
-      {/* Sign-Out button */}
-      <li className="border-b-4 py-2 text-center">
-        <button className="cursor-pointer">Sign-Out</button>
-      </li>
-
-      {/* Sign-In button */}
-      <li className="border-b-4 py-2 text-center">
-        <button className="cursor-pointer">Sign-In</button>
-      </li>
+      {session?.user ? (
+        /* Sign-Out button */
+        <li className="border-b-4 py-2 text-center">
+          <button
+            className="cursor-pointer"
+            onClick={() => signOut()}>
+            Sign-Out
+          </button>
+        </li>
+      ) : (
+        /* Sign-In button */
+        <li className="border-b-4 py-2 text-center">
+          <button
+            className="cursor-pointer"
+            onClick={() => signIn()}>
+            Sign-In
+          </button>
+        </li>
+      )}
     </ul>
   )
 }
