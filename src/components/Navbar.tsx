9dbfbaf0e059388adc2d22d9b050adb7a4863ca6
Han-Bin Song <thdgksq@naver.com>
2025-04-07 01:50:26 +0900

    useSession에서 직접 만든 함수로 세션 데이터 가져오도록 변경

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,13 @@
 import Link from 'next/link'
 import { useState } from 'react'
 import NavItem from './NavItem'
+import { User } from '@prisma/client'
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
   const [menu, setMenu] = useState(false)
 
   const handleMenu = () => {
@@ -30,13 +35,18 @@ const Navbar = () => {
 
         {/* navItem large screen */}
         <div className="hidden sm:block">
-          <NavItem />
+          <NavItem currentUser={currentUser} />
         </div>
       </div>
 
       {/* navItem mobile */}
       <div className="block sm:hidden">
-        {menu === false ? null : <NavItem mobile />}
+        {menu === false ? null : (
+          <NavItem
+            mobile
+            currentUser={currentUser}
+          />
+        )}
       </div>
     </nav>
   )
--- src/serverActions/auth.ts
+++ src/serverActions/auth.ts
@@ -1,9 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use server'
 import { auth, signIn, signOut, update } from '@/auth'
+import { prisma } from '@/libs/prismadb'
 
 export const signOutWithForm = async (formData: FormData) => {
   await signOut()
 }
 
 export { auth as getSession, update as updateSession }
+
+export default async function getCurrentUser() {
+  try {
+    const session = await auth()
+
+    if (!session?.user?.email) return null
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email
+      }
+    })
+
+    if (!currentUser) return null
+
+    return currentUser
+  } catch (err) {
+    return null
+  }
+}
